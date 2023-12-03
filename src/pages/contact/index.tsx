f62f09f89514f7952a558b62f3d00fba0dd9bec3
Сергей Блохин <sblohin@cloudfactory.ru>
2023-12-03 10:35:19 +0300

    prepare

--- src/pages/contact/index.tsx
+++ src/pages/contact/index.tsx
@@ -1,6 +1,6 @@
 import { IsDev } from "@/components/isDev/IsDev";
 import RootLayout from "@/pages/RootLayout";
-import { HeaderSite } from "@/pages";
+import { HeaderSite } from "../../components/header/HeaderSite/HeaderSite";
 
 export default function Index() {
   return (
