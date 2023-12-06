498765a1a14c217cce040517c8738b8af04b5529
Сергей Блохин <sblohin@cloudfactory.ru>
2023-12-06 11:01:29 +0300

    bugFix

--- src/components/isDev/IsDev.tsx
+++ src/components/isDev/IsDev.tsx
@@ -1,14 +1,23 @@
-import { FC, PropsWithChildren } from "react";
+import React, { FC, PropsWithChildren } from "react";
 import { DEV } from "@/const";
+import { ClassesSx } from "@/theme/theme";
+import { Box, CircularProgress, Skeleton, Stack } from "@mui/material";
 
 interface Props {}
 
 export const IsDev: FC<PropsWithChildren<Props>> = ({ children }) => {
   if (DEV) {
     return (
-      <div style={{ textAlign: "center", fontSize: "60px" }}>
-        Сайт в разработке
-      </div>
+      <>
+        <div style={{ textAlign: "center", fontSize: "60px" }}>
+          Сайт в разработке
+        </div>
+        <Box display={"flex"} justifyContent={"center"} mt={5}>
+          <Stack sx={{ color: "grey.500" }} spacing={12} direction="row">
+            <CircularProgress size={100} color="success" />
+          </Stack>
+        </Box>
+      </>
     );
   }
   return <>{children}</>;
