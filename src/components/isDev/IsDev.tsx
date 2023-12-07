b15280792d44e229fd2eb9d6eeb5ac0ed208ff1e
Сергей Блохин <sblohin@cloudfactory.ru>
2023-12-07 10:51:00 +0300

    dev

--- src/components/isDev/IsDev.tsx
+++ src/components/isDev/IsDev.tsx
@@ -4,6 +4,7 @@ import { ClassesSx } from "@/theme/theme";
 import { Box, CircularProgress, Skeleton, Stack } from "@mui/material";
 import Image from "next/image";
 import { montserrat } from "@/components/header/HeaderSite/HeaderSite";
+import ErrorLoader from "next/dist/build/webpack/loaders/error-loader";
 
 interface Props {}
 
@@ -15,15 +16,19 @@ export const IsDev: FC<PropsWithChildren<Props>> = ({ children }) => {
           В разработке
         </Box>
         <Box sx={c.setcionGeneralPageBanner}>
+          <Skeleton />
           <Image
             src="/photoNew/dev.png"
             alt={"image"}
             layout={"fill"}
+            // sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"
             objectFit="contain"
             objectPosition="top center" // По умолчанию 'center', но может быть изменено
             quality={100}
             placeholder={"blur"}
+            // priority
             blurDataURL={"/photoNew/devPre.jpg"}
+            onLoad={(e) => console.log(e.target)}
           />
         </Box>
       </>
