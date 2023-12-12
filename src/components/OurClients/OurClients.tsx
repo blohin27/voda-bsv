309edaa66f0a1d2e37441ba9db085649aa26e6f6
Сергей Блохин <sblohin@cloudfactory.ru>
2023-12-12 07:55:52 +0300

    fix

--- src/components/OurClients/OurClients.tsx
+++ src/components/OurClients/OurClients.tsx
@@ -17,50 +17,50 @@ export const OurClients = () => {
   const [thumbsSwiper, setThumbsSwiper] = useState<SwiperClass | null>(null);
   return (
     <Box>
-      {/*<Box style={{ maxWidth: "1200px", margin: "0 auto" }}>*/}
-      {/*  <TitleComponent title={"Наши клиенты"} />*/}
-      {/*  <Box sx={classes.desc}>*/}
-      {/*    Наши клиенты такие как: Emirates, Газпром, Seven Suns Development, Bitriver, Натура Сиберика, автоцентры "КИА", Rybakov PlaySchool, сеть магазинов*/}
-      {/*    "Пятёрочка" , Альфа Банк, Банк ВТБ, Посольство Республики Гана, Университет прокуратуры Российской Федерации им. Дмитрия Герца, Пансионат «Никольский*/}
-      {/*    парк» в Зеленограде , ФСИН России по г. Москве.*/}
-      {/*  </Box>*/}
-      {/*  <Swiper*/}
-      {/*    navigation*/}
-      {/*    // pagination={{ type: "fraction" }}*/}
-      {/*    modules={[Navigation, Pagination]}*/}
-      {/*    onSwiper={(swiper) => console.log(swiper)}*/}
-      {/*    thumbs={{*/}
-      {/*      swiper: thumbsSwiper,*/}
-      {/*    }}*/}
-      {/*    className="h-96 w-full rounded-lg"*/}
-      {/*  >*/}
-      {/*    {[*/}
-      {/*      { in: 1, r: "png" },*/}
-      {/*      { in: 2, r: "png" },*/}
-      {/*      { in: 3, r: "svg" },*/}
-      {/*      { in: 4, r: "svg" },*/}
-      {/*      { in: 5, r: "png" },*/}
-      {/*    ].map((item, index) => (*/}
-      {/*      <SwiperSlide key={index}>*/}
-      {/*        <Box style={{ display: "flex", justifyContent: "center", flexDirection: "column", alignItems: "center" }}>*/}
-      {/*          <Box sx={classes.wrapperItem}>*/}
-      {/*            <Image*/}
-      {/*              src={`/photoNew/${item.in}.${item.r}`}*/}
-      {/*              alt={"image"}*/}
-      {/*              layout={"fill"}*/}
-      {/*              // layout="responsive"*/}
-      {/*              // height={200}*/}
-      {/*              // width={200}*/}
-      {/*              objectFit="contain"*/}
-      {/*              objectPosition="center center" // По умолчанию 'center', но может быть изменено*/}
-      {/*              quality={100}*/}
-      {/*            />*/}
-      {/*          </Box>*/}
-      {/*        </Box>*/}
-      {/*      </SwiperSlide>*/}
-      {/*    ))}*/}
-      {/*  </Swiper>*/}
-      {/*</Box>*/}
+      <Box style={{ maxWidth: "1200px", margin: "0 auto" }}>
+        <TitleComponent title={"Наши клиенты"} />
+        <Box sx={classes.desc}>
+          Наши клиенты такие как: Emirates, Газпром, Seven Suns Development, Bitriver, Натура Сиберика, автоцентры КИА, Rybakov PlaySchool, сеть магазинов
+          Пятёрочка , Альфа Банк, Банк ВТБ, Посольство Республики Гана, Университет прокуратуры Российской Федерации им. Дмитрия Герца, Пансионат «Никольский
+          парк» в Зеленограде , ФСИН России по г. Москве.
+        </Box>
+        <Swiper
+          navigation
+          // pagination={{ type: "fraction" }}
+          modules={[Navigation, Pagination]}
+          onSwiper={(swiper) => console.log(swiper)}
+          thumbs={{
+            swiper: thumbsSwiper,
+          }}
+          className="h-96 w-full rounded-lg"
+        >
+          {[
+            { in: 1, r: "png" },
+            { in: 2, r: "png" },
+            { in: 3, r: "svg" },
+            { in: 4, r: "svg" },
+            { in: 5, r: "png" },
+          ].map((item, index) => (
+            <SwiperSlide key={index}>
+              <Box style={{ display: "flex", justifyContent: "center", flexDirection: "column", alignItems: "center" }}>
+                <Box sx={classes.wrapperItem}>
+                  <Image
+                    src={`/photoNew/${item.in}.${item.r}`}
+                    alt={"image"}
+                    layout={"fill"}
+                    // layout="responsive"
+                    // height={200}
+                    // width={200}
+                    objectFit="contain"
+                    objectPosition="center center" // По умолчанию 'center', но может быть изменено
+                    quality={100}
+                  />
+                </Box>
+              </Box>
+            </SwiperSlide>
+          ))}
+        </Swiper>
+      </Box>
     </Box>
   );
 };
