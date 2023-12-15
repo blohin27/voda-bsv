b8e0792412a97a4d6399631de90d8f16dfff99e4
Сергей Блохин <sblohin@cloudfactory.ru>
2023-12-15 15:24:55 +0300

    com1

--- src/config/nodemailer.ts
+++ src/config/nodemailer.ts
@@ -2,6 +2,7 @@ import nodemailer from "nodemailer";
 
 const email = process.env.EMAIL;
 const pass = process.env.EMAIL_PASS;
+// const pass = process.env.EMAIL_PASS;
 
 export const transporter = nodemailer.createTransport({
   service: "gmail",
