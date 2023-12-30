85317c25883bf31563ebe7b7e623fc44cb1c61a8
Сергей Блохин <sblohin@cloudfactory.ru>
2023-12-30 13:14:14 +0300

    prepare

--- src/const.ts
+++ src/const.ts
@@ -4,5 +4,5 @@ export const MOBILE_TELEPHONE: string = "7-925-731-81-52";
 export const IP = { name: "ИП Блохин Д.В.", OGRNIP: "123456789123456", INN: "123456789122" };
 export const EMAIL: string = "info@voda-bsv.ru";
 export const NICKTELEGRAM = "blohinsergei";
-export const DEV = false;
+export const DEV = true;
 export const INN = "";
