0e0e47a09f3c735205f42c4982b0d818241be1d2
Сергей Блохин <sblohin@cloudfactory.ru>
2023-12-31 10:08:15 +0300

    prepare

--- src/const.ts
+++ src/const.ts
@@ -1,5 +1,6 @@
 // export const COLOR_MILK = "#fcfbf9";
 export const COLOR_MILK = "#edfaff";
+export const COLOR_ORANGE = "#fe6c36";
 export const MOBILE_TELEPHONE: string = "7-925-731-81-52";
 export const IP = { name: "ИП Блохин Д.В.", OGRNIP: "123456789123456", INN: "123456789122" };
 export const EMAIL: string = "info@voda-bsv.ru";
